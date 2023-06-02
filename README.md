dba1cb86a1b3ceb5805194f1af1cda55e5d011b9
unknown <jyanggi888@gmail.com>
2023-06-02 16:48:42 +0800

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# little-lemon-menu-kotlin 
---
+++ app/src/androidTest/java/com/littlelemon/menu/FilterHelperTest.kt
@@ -0,0 +1,26 @@
+package com.littlelemon.menu
+import org.junit.Assert.assertEquals
+import org.junit.Test
+class FilterHelperTest {
+
+
+    private val testFilterHelper: FilterHelper = FilterHelper()
+    @Test
+    fun filterProducts_filterTypeDessert_croissantReturned() {
+        val sampleProductsList = mutableListOf(
+            ProductItem(title = "Black tea", price = 3.00, category = "Drinks", R.drawable.black_tea),
+            ProductItem(title = "Croissant", price = 7.00, category = "Dessert", R.drawable.croissant),
+            ProductItem(title = "Bouillabaisse", price = 20.00, category = "Food", R.drawable.bouillabaisse)
+        )
+        val result = testFilterHelper.filterProducts(
+            FilterType.Dessert,
+            sampleProductsList
+        )
+
+        assertEquals(1, result.size)
+        assertEquals("Croissant", result[0].title)
+
+
+    }
+
+}
